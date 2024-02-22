4686038c9b25303a97570296470ad6de10b8e16b
amit <amit25685@gmail.com>
2024-02-22 21:28:21 +0530

    Added custom component
    Added custom component

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
 }
